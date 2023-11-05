0f469a62d04567d0d99e2fd91be01058e44b7127
Deemantha Magodu <magodu@adobe.com>
2023-11-06 00:43:58 +0530

    pushing dynamic block

---
+++ blocks/dynamic-articles/dynamic-articles.js
@@ -0,0 +1,14 @@
+export default async function decorate(block) {
+
+    const resp = await fetch('query-index.json');
+    const json = await resp.json();
+    const ulEl = document.createElement("ul");
+    json.data.forEach(element => {
+        console.log(element);
+            const liEl = document.createElement("li");
+            liEl.innerText = element.title;
+            ulEl.appendChild(liEl);
+
+    });
+    block.appendChild(ulEl);
+}
